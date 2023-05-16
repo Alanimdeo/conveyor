d14a8eb11a8bb48174a37a7df0c8745ac673a91b
Alanimdeo <alan@imdeo.kr>
2023-05-16 10:52:40 +0900

    preCommit.js 수정

--- scripts/preCommit.js
+++ scripts/preCommit.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
-const packageJson = require("./package.json");
-const backendPackageJson = require("./backend/package.json");
-const frontendPackageJson = require("./frontend/package.json");
+const packageJson = require(path.join(__dirname, "../package.json"));
+const backendPackageJson = require(path.join(__dirname, "../backend/package.json"));
+const frontendPackageJson = require(path.join(__dirname, "../frontend/package.json"));
 
 backendPackageJson.version = packageJson.version;
 frontendPackageJson.version = packageJson.version;
 
-fs.writeFileSync("./backend/package.json", JSON.stringify(backendPackageJson, null, 2));
-fs.writeFileSync("./frontend/package.json", JSON.stringify(frontendPackageJson, null, 2));
+fs.writeFileSync(path.join(__dirname, "../backend/package.json"), JSON.stringify(backendPackageJson, null, 2));
+fs.writeFileSync(path.join(__dirname, "../frontend/package.json"), JSON.stringify(frontendPackageJson, null, 2));
+
+try {
+  execSync("git add ./**/package.json", { cwd: path.join(__dirname, "..") });
+} catch (err) {
+  console.error(err);
+}
