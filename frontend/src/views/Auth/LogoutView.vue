1e258d785903bfe3146ef381f0cd20854787843b
Alanimdeo <alan@imdeo.kr>
2023-09-25 08:53:39 +0900

    WebKit 환경에서 로그아웃이 제대로 이루어지지 않음 #35 해결

--- frontend/src/views/Auth/LogoutView.vue
+++ frontend/src/views/Auth/LogoutView.vue
@@ -7,6 +7,10 @@ const router = useRouter();
 
 await fetch("/api/logout", {
   method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({}),
 });
 document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
